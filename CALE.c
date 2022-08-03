6652159deb1389b49b4d3ce32e802a469dfdfea1
KHADIJA24B <khadijabouabid23@gmail.com>
2022-08-03 11:28:28 +0200

    Create CALE.c
    gg

---
+++ CALE.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+    int A , B;
+    char operation;
+    printf(" entre le nomre A : ");
+    scanf("%d",&A);
+    printf(" entre le operation : ");
+    scanf("%d",&B);
+    printf(" entre le nomre B : ");
+    scanf 
+
+}
